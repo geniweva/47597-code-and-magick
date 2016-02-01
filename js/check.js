3a482662d974ab52cb6b847dc4146b588d868049
geniweva <geniweva@mail.ru>
2016-02-01 18:01:24 +0200

    module1-task2
    исправленная версия

--- js/check.js
+++ js/check.js
@@ -1,31 +1,44 @@
 
-   
-function getMessage(a, b){
-   var sum = 0;
-   var message = 0;
-    
-   if((typeof a) == "Boolean"){
-       if(a == true){
-           message = 'Я попал в' + ' ' + b;
-       }
-       else{
-           message = 'Я никуда не попал';
-       }
-   }
-   else(if((typeof a) == 'Number')){
-       message = 'Я прыгнул на ' + '' + (a * 100) + 'сантиметров';
-   }
-   else(if((typeof a) == 'Object')){
-       for(var i = 0; i < a.length; i++){
-        var sum = sum + a[i]; 
-     }
-       message = 'Я прошел ' + sum + ' шагов';
-   }
-   else(if((typeof a == 'Object')) && ((typeof b)) == 'Object'){
-       for(var n = 0; n < a.length && n < b.length; n++){
-                sum = sum + (a[n]*b[n]);               
-        }
-       message = 'Я прошел ' + sum + 'метров'; 
-   }            
-   return message; 
-};
+// Строгмй метод
+'use strict';
+
+// getMessage(a:*, b:*=):string
+function getMessage( a, b ) {
+
+	var message;
+	var sum = 0;
+	var i;
+
+	if ( ( typeof a ) === 'boolean' ) {
+        
+		if ( a ) {
+			message = 'Я попал в ' + b;
+
+		} else {
+
+			message = 'Я никуда не попал';
+		}
+
+	} else if ( ( typeof a ) === 'number' ) {
+
+		message = 'Я прыгнул на ' + (a * 100) + ' сантиметров';
+
+	} else if ( Array.isArray( a ) && Array.isArray( b ) ) {
+        
+		for ( i = 0; i < a.length && i < b.length; i++ ) {
+			sum += ( a[i] * b[i] );
+		}
+		message = 'Я прошёл ' + sum + ' метров';
+
+	} else if ( Array.isArray( a ) ) {
+
+		for ( i = 0 ; i < a.length; i++ ) {
+			sum += a[i];
+		}
+
+		message = 'Я прошёл ' + sum + ' шагов';
+	}
+
+	return message;
+}
+
