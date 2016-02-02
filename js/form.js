ed1c0a69d8390cf87e78ab90f35d9fb55cf84252
geniweva <geniweva@mail.ru>
2016-02-02 12:34:34 +0200

    module3-task1

--- js/form.js
+++ js/form.js
@@ -9,61 +9,36 @@
     evt.preventDefault();
     formContainer.classList.remove('invisible'); 
  //мой код 
+     var myBut = document.querySelector('.review-form-control'); // находим кнопку
+     myBut.disabled = "true";
+     var elem_1 = document.querySelector('#review-mark-1'); //нахожу оценку 1
+     var elem_2 = document.querySelector('#review-mark-2'); //нахожу оценку 2
+     var myText = document.querySelector('#review-text');  //нахожу textarea;
+    // var fieldName = document.querySelector('.review-fields-name'); // нахожу поля имя и отзыв
+    // var fieldV = document.querySelector('.review-fields-text');  
       
-      var elem_1 = document.querySelector('#review-mark-1').value;
-      var elem_2 = document.querySelector('#review-mark-2').value;
-      var elemText = document.querySelector('#review-text'); 
+     var elems = document.querySelectorAll('.review-mark-label'); //нахожу все пять оценок 
       
-      if(elemText.value == null && elem_1 == 1){
-           elemTest.setAttribute('required', 'required');
-      }else if(elemText.value == null && elem_2 == 2){
-          elemTest.setAttribute('required', 'required');
+    if(document.querySelector('#review-name') == null){                                              //если поле имя пусто, то textarea - обязателен
+        myText.required = "true";
+    }else{
+        myText.requerd = "false";                                                                     // в противном случае
+        document.removeClass('.review-fields-name');                                                  //удаляем поле имя
+    }   
+      
+      
+      for(var i = 0; i < elems.length; i++){                                                          // перебираю все оценки 
+          elems[i].onclick = function(){                                                              // если 1-й элемент =1 или =2 и поле текст пусто
+              if( (elems[i] == elem_1 && myText == null) || (elems[i] == elem_2 && myText == null) ){   // то поле текс становится обязательным
+                  myText.required = "true"; 
+              }else {
+                  myText.required = "false";                                                            // в противном случае снимаем обязательность поля
+                  myBut.disabled = "false";                                                             // и даем возможность отправить   
+              }           
+          }
       }
       
-      
-         
-  
-      
-      
-// Никак не могу вообще просто добавить required
-      
-      
-      
-  // А это первые тоже неработающие версии    
-//      
-//     for (var j = 0; j < elemForm.length; j++){
-//         elemForm[j].onclick = function(){
-//            if(elem == nul || elem = ''){
-//                var elemText = document.querySelector('#review-text');
-//                elemText.innerHTML = required; 
-//                alert('Заполните поле текст');
-//                return false;
-//            }    
-//         }      
-//      }
-//      
     
-      
-      
-      //var elem = document.getElementsByName('review-mark');      
-      //elem = Array.prototype.slice.call(elem);
-      
-//      for(var j = 0; j < elem.length; j++){
-//         elem[j].onclick = function(){ 
-//          
-//          var elemValue = elem[j].value;
-//          if(elemValue == elem[0].getAttribute('value')){
-//              alert('я первая кнопка' + '' + elemValue);
-//              
-//          }
-//          else if(elemValue == elem[1].getAttribute('value')){
-//              alert('я вторая  кнопка' + '' + elemValue);
-//          }
-//             return;
-//         }
-//      }
-            
-     
 //конец моего кода  
   
   };
