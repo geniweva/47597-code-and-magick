daa8de06d7ea1eff1cee0be43385799f8f891365
geniweva <geniweva@mail.ru>
2016-02-09 18:42:05 +0200

    module3-task1

--- js/form.js
+++ js/form.js
@@ -8,57 +8,69 @@
   formOpenButton.onclick = function(evt) {
     evt.preventDefault();
     formContainer.classList.remove('invisible'); 
- //мой код 
-      var myBut = document.querySelector('.review-form-control');
-      var elems = document.querySelectorAll('.review-mark-label'); //нахожу все пять оценок 
-      var elem_1 = document.querySelector('#review-mark-1'); // нахожу 1-й элемент
-      var elem_2 = document.querySelector('#review-mark-2'); // нахожу 2-й элемент
-      myBut.disabled = "true";
-      var fieldName = document.querySelector('#review-name'); //нахожу поле имя
-      var fieldText = document.querySelector('#review-text');  //нахожу textarea; 
-      fieldName.required = "true";
-      
-      
-     
-     if(fieldName.value !== "" && fieldText.value !== ""){
-          document.querySelector('.review-fields').style.display='none';
-          myBut.removeAttribute("disabled");
-      }
-      if( (fieldName.value !== "") && (document.querySelector('.review-fields-name').display="block") ){
-          document.querySelector('.review-fields-name').style.display="none";
-      }else if( (fieldName.value == "") && (document.querySelector('.review-fields-name').display="none") ){
-          document.querySelector('.review-fields-name').style.display="block";
-      }
-      if( (fieldText.value !== "") && (document.querySelector('.review-fields-text').display="block") ){
-          document.querySelector('.review-fields-text').style.display="none";
-      }else if( (fieldText.value == "") && (document.querySelector('.review-fields-text').display="none") ){
-          document.querySelector('.review-fields-text').style.display="block";
-      }
-      for (var i = 0; i < elems.length; i++){
-          elems[i].onclick = function(){
-              if(elems[i].innerText == elem_1.value || elems[i].innerText == elem_2.value){
-                 fieldText.required = "true";          
-              }else{
-                fieldText.required = "false";    
-              } 
-              
-          }
-      }
-      
-    
-//конец моего кода  
-  
   };
-
+    var myBut = document.querySelector('.review-form-control');
+    var leftText = document.querySelector('.review-fields-text');
+    var leftName = document.querySelector('.review-fields-name');
+    var elems = document.querySelectorAll('.review-mark-label');
+    var fieldName = document.querySelector('#review-name');
+    var fieldText = document.querySelector('#review-text');
+    
+    myBut.disabled = 'true';
+    fieldName.required = 'true';
+    leftText.classList.add('invisible');
+    
+for (var i = 0; i < elems.length; i++){
+    elems[i].onclick = function(){
+        var rating = this.innerHTML;
+            if(rating < 3){
+                if(!validText()){
+                    leftText.classList.remove('invisible');
+                    myBut.disabled = 'true';
+                    fieldText.required = 'true';
+                }              
+                if(!validName() ){
+                    leftName.classList.remove('invisible');
+                    myBut.disabled = 'true';
+                }
+                
+            } else if(rating >= 3){
+                   leftText.classList.add('invisible'); 
+                   leftName.classList.remove('invisible');
+              }
+    }
+            
+}
+    
+fieldName.oninput = function(){
+    leftName.classList.add('invisible');
+    myBut.removeAttribute('disabled');
+}
+fieldText.oninput = function(){
+    leftText.classList.add('invisible');
+    myBut.removeAttribute('disabled');
+}
+    
+    function validName() {
+        if(fieldName.value === ''){
+            return false;
+        } else {
+            return true;
+        }
+    }
+    function validText() {
+        if(fieldText.value === ''){
+            return false;
+        } else {
+            return true;
+        }
+    }
+ 
   formCloseButton.onclick = function(evt) {
     evt.preventDefault();
     formContainer.classList.add('invisible');
   };
-    
-    
-    
-    
-    
+      
 })();
   
       
